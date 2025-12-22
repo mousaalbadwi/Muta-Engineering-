a70ac44c13472637dadaa29f4a50c359f784600f
moeyad2003@gmail.com <moeyad2003@gmail.com>
2025-12-22 18:35:09 +0300

    Add a /Simulator/Circuit featuer and edit an Electiacal dep and computer dep(add  button to go to /Simulator/Circuit)

---
+++ Controllers/SimulatorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MutaEngineering.Controllers
+{
+    public class SimulatorController : Controller
+    {
+        public IActionResult Circuit()
+        {
+            return View();
+        }
+    }
+}
